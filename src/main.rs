99f55bd5e8b5d95f60dbaabdba93d2a8de9ac410
ludovicm67 <ludovicmuller1@gmail.com>
2019-03-16 00:14:15 +0100

    created basic portal

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate rouille;
+
+use rouille::Response;
+
+const CREDENTIALS: [(&'static str, &'static str); 2] = [("admin", "password"), ("teddy", "bear")];
+
+fn main() {
+    let web_content = include_str!("index.html");
+    rouille::start_server("0.0.0.0:4280", move |request| {
+        if let Ok(input) = post_input!(request, { username: String, password: String }) {
+            if !CREDENTIALS.contains(&(input.username.as_str(), input.password.as_str())) {
+                return Response::text("Invalid credentials").with_status_code(401);
+            }
+
+            let resp = Response::text(format!(
+                "Hello {}! You are now logged in and can browse the web!",
+                input.username
+            ));
+            let next = request.get_param("next");
+            if let Some(next) = next {
+                resp.with_additional_header("Location", next)
+                    .with_status_code(301)
+            } else {
+                resp
+            }
+        } else {
+            Response::html(web_content)
+        }
+    });
+}
