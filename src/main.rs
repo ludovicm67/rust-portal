cbacdc58c80198e6233dac01d24a4c0ebfedc16f
ludovicm67 <ludovicmuller1@gmail.com>
2019-03-16 17:14:50 +0100

    log + redirect with code

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,33 @@
 extern crate rouille;
 
 use rouille::Response;
+use std::io;
 
 const CREDENTIALS: [(&'static str, &'static str); 2] = [("admin", "password"), ("teddy", "bear")];
 
 fn main() {
     let web_content = include_str!("index.html");
     rouille::start_server("0.0.0.0:4280", move |request| {
-        if let Ok(input) = post_input!(request, { username: String, password: String }) {
-            if !CREDENTIALS.contains(&(input.username.as_str(), input.password.as_str())) {
-                return Response::text("Invalid credentials").with_status_code(401);
-            }
+        rouille::log(request, io::stdout(), || {
+            if let Ok(input) = post_input!(request, { username: String, password: String }) {
+                if !CREDENTIALS.contains(&(input.username.as_str(), input.password.as_str())) {
+                    return Response::text("Invalid credentials").with_status_code(401);
+                }
 
-            let resp = Response::text(format!(
-                "Hello {}! You are now logged in and can browse the web!",
-                input.username
-            ));
-            let next = request.get_param("next");
-            if let Some(next) = next {
-                resp.with_additional_header("Location", next)
-                    .with_status_code(301)
+                let resp = Response::text(format!(
+                    "Hello {}! You are now logged in and can browse the web!",
+                    input.username
+                ));
+                let next = request.get_param("next");
+                if let Some(next) = next {
+                    resp.with_additional_header("Location", next)
+                        .with_status_code(301)
+                } else {
+                    resp
+                }
             } else {
-                resp
+                Response::html(web_content)
             }
-        } else {
-            Response::html(web_content)
-        }
+        })
     });
 }
