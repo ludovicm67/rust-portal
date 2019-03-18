9a90c8a76986eb01c850527c63a810e365053c15
ludovicm67 <ludovicmuller1@gmail.com>
2019-03-18 19:58:18 +0100

    support for next param

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 #[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_qs as qs;
+
+#[macro_use]
 extern crate rouille;
 
 use rouille::Response;
+use serde::{Deserialize, Serialize};
 use std::io;
 use std::process::Command;
+
+#[derive(PartialEq, Serialize, Deserialize)]
+struct Params {
+    next: String,
+}
 
 const CREDENTIALS: [(&'static str, &'static str); 2] = [("admin", "password"), ("teddy", "bear")];
 
@@ -11,11 +22,14 @@ fn main() {
     let web_content = include_str!("index.html");
     rouille::start_server("0.0.0.0:4280", move |request| {
         rouille::log(request, io::stdout(), || {
+            let params: Option<Params> = qs::from_str(request.raw_query_string()).ok();
+
             if let Ok(input) = post_input!(request, { username: String, password: String }) {
                 if !CREDENTIALS.contains(&(input.username.as_str(), input.password.as_str())) {
                     return Response::text("Invalid credentials").with_status_code(401);
                 }
-                Command::new("pfctl")
+
+                let child = Command::new("pfct")
                     .args(&[
                         "-t",
                         "whitelist",
@@ -24,20 +38,43 @@ fn main() {
                         &request.remote_addr().ip().to_string(),
                     ])
                     .spawn();
+
+                if let Err(_) = child {
+                    return Response::text("internal server error").with_status_code(500);
+                }
+
                 let resp = Response::text(format!(
                     "Hello {}! You are now logged in and can browse the web!",
                     input.username
                 ));
-                let next = request.get_param("next");
-                if let Some(next) = next {
-                    resp.with_additional_header("Location", next)
-                        .with_status_code(301)
-                } else {
-                    resp
+
+                if let Some(params) = params {
+                    return resp
+                        .with_additional_header("Location", params.next)
+                        .with_status_code(302);
                 }
-            } else {
-                Response::html(web_content)
+
+                return resp;
             }
+
+            if params == None {
+                let next = format!(
+                    "http://{}{}",
+                    request.header("Host").unwrap_or("localhost"),
+                    request.raw_url()
+                );
+                let query = qs::to_string(&Params {
+                    next: String::from(next),
+                });
+                return match query {
+                    Ok(q) => Response::text("Redirecting")
+                        .with_additional_header("Location", format!("http://192.168.10.1/?{}", q))
+                        .with_status_code(302),
+                    Err(_) => Response::text("internal server error").with_status_code(500),
+                };
+            }
+
+            Response::html(web_content)
         })
     });
 }
